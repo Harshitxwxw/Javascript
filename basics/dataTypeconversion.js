3b93afb43727c7a64c5bd17c0c54f1c098fdc119
Harshitxwxw <harshitmagarde12@gmail.com>
2025-05-15 09:45:02 +0000

    changes saved

--- basics/dataTypeconversion.js
+++ basics/dataTypeconversion.js
@@ -8,12 +8,12 @@ let converted_state= Number(state)
 let converted_space = Number(space)
 let con_bool = Number(bool)
 
-console.log(typeof score)
-console.log(typeof valueIn_number)
-console.log( valueIn_number)
-console.log( converted_state)
-console.log( converted_space)
-console.log( con_bool)
+// console.log(typeof score)
+// console.log(typeof valueIn_number)
+// console.log( valueIn_number)
+// console.log( converted_state)
+// console.log( converted_space)
+// console.log( con_bool)
 
 
 //  "33" =>  33
@@ -25,7 +25,44 @@ let isLoggedin = 1
 let str = "w"
 let someNumber = 78
 
-console.log( String(isavailable))   //  "true"
-console.log( Boolean(isLoggedin))   //  1 =>true -> boolean ,  0 => false
-console.log( Boolean(str))          //  empty string "" => false , otherwise => true
-console.log( String(someNumber))    //  "78"
+// console.log( String(isavailable))   //  "true"
+// console.log( Boolean(isLoggedin))   //  1 =>true -> boolean ,  0 => false
+// console.log( Boolean(str))          //  empty string "" => false , otherwise => true
+// console.log( String(someNumber))    //  "78"
+
+
+
+//   Operations
+
+// let value = 45
+// let negvalue = -value
+// console.log( negvalue) 
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(4/3)
+// console.log(2%3)
+// console.log(((3+4)*5) % 3);
+// console.log(true+true);
+
+
+let str1 = "Hello"
+let str2 = " World"
+
+// console.log(str1 + str2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 +2)
+// console.log(1 + 2 +"2")
+// console.log(1 + "2" + 2)
+
+// console.log(+true)
+// console.log(+"")
+
+let counter = 90
+console.log(++counter);  //  91
+console.log(counter++);  //  91
+
+
