cac312fd4a7e08d5e3b0534e60cb216cb2cf13f1
Harshitxwxw <harshitmagarde12@gmail.com>
2025-05-15 08:41:51 +0000

    changed saved

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  
+
+// alert(3+3)   // we are using nodejs, not browser
+console.log(3+3)
+
+console.log("Hello") 
+
+
+let nam = "Hello"       // string  => ""
+let age = 18             // integer
+let isLoggedin = true    //  boolean => True/ false
+let state = null         //  null => standlone value  
+//  undefined =>
+// symbol => unique
+// object
+
+console.log(typeof nam);   
+console.log(typeof age);
+console.log(typeof isLoggedin);  
+console.log(typeof state);    // object
+console.log(typeof undefined);   // undefined
