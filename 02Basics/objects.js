71a664197c0bc39e3b3d8a609d518c74dcfc596e
Harshitxwxw <harshitmagarde12@gmail.com>
2025-05-17 09:28:40 +0000

    changes saved

--- 02Basics/objects.js
+++ 02Basics/objects.js
@@ -1,4 +1,3 @@
-// singleton
 
 // object literals
 const mySym = Symbol("key1")
@@ -27,18 +26,82 @@ JSuser.email = 'harshit@gmail.com'   // it will update the email
 // Object.freeze(JSuser)    // it will freeze the user i.e it's data connot be changed
 JSuser.email = 'harshit@microsoft.com'
 
-console.log(JSuser)
+// console.log(JSuser)
 
 
 JSuser.greetings = function(){
-    console.log("Hello js user");
+    // console.log("Hello js user");
     
 }
 JSuser.greetingsTwo = function(){
-    console.log(`Hello js user,${this.name}`);
+    // console.log(`Hello js user,${this.name}`);
     
 }
 
 
-console.log(JSuser.greetings());
-console.log(JSuser.greetingsTwo());
+// console.log(JSuser.greetings());
+// console.log(JSuser.greetingsTwo());
+
+
+
+
+//  singelton object
+// const tinderUser = new Object()
+
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Shubham"
+tinderUser.isLoggedin = true
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email : "Some@gmail.com",
+    fullname : {
+        userFullName : {
+            firstName : "Harshit",
+            lastName : "Magarde"
+        }
+    }
+}
+// console.log(regularUser.fullname?.userFullName.lastName);
+
+
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "a", 4 : "b"}
+const obj4 = {5 : "a", 6 : "b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+
+// array of objects
+const users = [
+        {
+            id : 1,
+            email : "Harsh@gmail.com"
+        },
+        {
+            id : 1,
+            email : "Harsh@gmail.com"
+        },
+        {
+            id : 1,
+            email : "Harsh@gmail.com"
+        }
+]
+
+console.log(users[1].email);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));   //  true
+console.log(tinderUser.hasOwnProperty('age'));     // false
+
+
