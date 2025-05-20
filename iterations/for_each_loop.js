a806ca609db5acc13b8266da274d188145b2afd0
Harshitxwxw <harshitmagarde12@gmail.com>
2025-05-20 11:04:18 +0000

    changes saved

--- iterations/for_each_loop.js
+++ iterations/for_each_loop.js
@@ -6,6 +6,40 @@ lang.forEach(function (item) {
 })
 
 lang.forEach( (item) => {
+    // console.log(item);
+})
+
+function printMe(item) {
     console.log(item);
+}
+
+// lang.forEach(printMe)
+
+
+lang.forEach( (item, index, arr) => {
+    // console.log(item,index,arr);
+})
+
+
+
+const myCoding = [
+    {
+        langName : "JavaScript",
+        langFile : "js"
+    },
+    {
+        langName : "C++",
+        langFile : "cpp"
+    },
+    {
+        langName : "Python",
+        langFile : "py"
+    }
+]
+
+
+myCoding.forEach( (item) =>{
+    console.log(item.langName);
     
+    // console.log(`Language Name is : ${item.langName} & Language File is : ${item.langFile}` );
 })
